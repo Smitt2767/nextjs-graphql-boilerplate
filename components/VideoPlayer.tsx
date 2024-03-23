a887b63a879ee108a21b50e1fdd09cef88429bde
smit-vekariya-us <smit@cappital.co>
2024-03-23 13:12:35 +0530

    fullscreen

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import { cn, secondsToFormattedHMS } from '@/lib/utils';
-import { Play, type LucideIcon, PictureInPicture2, type LucideProps, Pause } from 'lucide-react';
+import {
+  Play,
+  type LucideIcon,
+  PictureInPicture2,
+  type LucideProps,
+  Pause,
+  Maximize,
+  Minimize
+} from 'lucide-react';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import React, {
@@ -11,10 +19,13 @@ import React, {
   useContext,
   useEffect,
   useMemo,
+  useRef,
   useState
 } from 'react';
 import ReactPlayer from 'react-player';
 import { type OnProgressProps } from 'react-player/base';
+import screenfull from 'screenfull';
+
 const ReactLazyPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 type VideoPlayerProps = {
@@ -40,11 +51,14 @@ type PlayerState = {
   loadedSeconds: number;
   loaded: number;
   duration: number;
+  isFullscreen: boolean;
 };
 
 type EmptyFunc = () => void;
 
 type PlayerContextProps = PlayerState & {
+  containerRef: React.RefObject<HTMLDivElement>;
+  title: string;
   isPlayerLoaded: boolean;
   onPlay: EmptyFunc;
   onPause: EmptyFunc;
@@ -54,6 +68,7 @@ type PlayerContextProps = PlayerState & {
   togglePip: EmptyFunc;
   forward: EmptyFunc;
   backward: EmptyFunc;
+  toggleFullscreen: EmptyFunc;
   onReady: (player: ReactPlayer) => void;
   seekTo: (seekAmount: number) => void;
   onProgress: (progress: OnProgressProps) => void;
@@ -94,62 +109,112 @@ const Button = ({ icon: Icon, iconProps, className, children, ...props }: Button
   );
 };
 
+const TopControls = () => {
+  const { playing, title } = usePlayer();
+
+  return (
+    <div
+      className={cn(
+        'absolute left-0 right-0 top-0 z-20 flex items-center gap-2 px-3 py-2 text-white md:px-4 md:py-3',
+        'opacity-0 group-focus-within:opacity-100 group-hover:opacity-100',
+        '-translate-y-full group-focus-within:translate-y-0 group-hover:translate-y-0',
+        'transition-[transform,_opacity] duration-150',
+        'flex items-center gap-4',
+        {
+          'translate-y-0 opacity-100': !playing
+        }
+      )}
+      onClick={(e) => e.stopPropagation()}
+    >
+      <p className="truncate text-base md:text-xl">{title}</p>
+    </div>
+  );
+};
+
+const CenterControls = () => {
+  const { playing, togglePlay } = usePlayer();
+
+  return (
+    <div
+      className={cn(
+        'xs:gap-6 absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2 text-white md:gap-16',
+        'opacity-0 transition-opacity duration-150 group-focus-within:opacity-100 group-hover:opacity-100',
+        {
+          'opacity-100': !playing
+        }
+      )}
+      onClick={(e) => e.stopPropagation()}
+    >
+      <Button
+        className="flex-shrink-0"
+        onClick={togglePlay}
+        icon={playing ? Pause : Play}
+        iconProps={{
+          className: 'size-[3em]'
+        }}
+      />
+    </div>
+  );
+};
+
+const BottomControls = () => {
+  const { playing, playedSeconds, duration, isFullscreen, togglePip, toggleFullscreen } =
+    usePlayer();
+
+  return (
+    <div
+      className={cn(
+        'absolute bottom-0 left-0 right-0 z-20 flex items-center gap-2 px-3 py-2 text-white md:px-4 md:py-3',
+        'opacity-0 group-focus-within:opacity-100 group-hover:opacity-100',
+        'translate-y-full group-focus-within:translate-y-0 group-hover:translate-y-0',
+        'transition-[transform,_opacity] duration-150',
+        {
+          'translate-y-0 opacity-100': !playing
+        }
+      )}
+      onClick={(e) => e.stopPropagation()}
+    >
+      {/* Left */}
+      <div className="flex flex-1 items-center gap-3">
+        {/* Video Played / Duration Info */}
+        <div className="flex items-center gap-1 text-xs md:text-sm">
+          <span>{secondsToFormattedHMS(playedSeconds)}</span>
+          <span>/</span>
+          <span className="text-white/60">{secondsToFormattedHMS(duration)}</span>
+        </div>
+      </div>
+      {/* Right */}
+      <div className="flex items-center gap-3">
+        {/* PIP */}
+        <Button
+          icon={PictureInPicture2}
+          iconProps={{
+            fill: 'transparent',
+            strokeWidth: 2
+          }}
+          onClick={togglePip}
+        />
+        <Button
+          icon={isFullscreen ? Minimize : Maximize}
+          iconProps={{
+            fill: 'transparent',
+            strokeWidth: 2
+          }}
+          onClick={toggleFullscreen}
+        />
+      </div>
+    </div>
+  );
+};
+
 const Controls = () => {
-  const { playing, playedSeconds, duration, togglePip, togglePlay } = usePlayer();
+  const { playing } = usePlayer();
 
   return (
     <>
-      <div
-        className={cn(
-          'xs:gap-6 absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2 text-white md:gap-16',
-          'opacity-0 transition-opacity duration-150 group-focus-within:opacity-100 group-hover:opacity-100',
-          {
-            'opacity-100': !playing
-          }
-        )}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <Button
-          className="flex-shrink-0"
-          onClick={togglePlay}
-          icon={playing ? Pause : Play}
-          iconProps={{
-            className: 'size-[3em]'
-          }}
-        />
-      </div>
-      <div
-        className={cn(
-          'absolute bottom-0 left-0 right-0 z-20 flex items-center gap-2 px-3 py-2 text-white md:px-4 md:py-3',
-          'opacity-0 group-focus-within:opacity-100 group-hover:opacity-100',
-          'translate-y-full group-focus-within:translate-y-0 group-hover:translate-y-0',
-          'transition-[transform,_opacity] duration-150',
-          {
-            'translate-y-0 opacity-100': !playing
-          }
-        )}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="flex flex-1 items-center gap-2">
-          {/* Video Played / Duration Info */}
-          <div className="flex items-center gap-1 text-xs md:text-sm">
-            <span>{secondsToFormattedHMS(playedSeconds)}</span>
-            <span>/</span>
-            <span className="text-white/60">{secondsToFormattedHMS(duration)}</span>
-          </div>
-        </div>
-        <div className="flex items-center gap-2">
-          {/* PIP */}
-          <Button
-            icon={PictureInPicture2}
-            iconProps={{
-              fill: 'transparent',
-              strokeWidth: 2
-            }}
-            onClick={togglePip}
-          />
-        </div>
-      </div>
+      <TopControls />
+      <CenterControls />
+      <BottomControls />
       <div
         className={cn(
           'pointer-events-none absolute inset-0 z-10 bg-black/40 opacity-0 transition-opacity duration-150 group-focus-within:opacity-100 group-hover:opacity-100',
@@ -161,11 +226,11 @@ const Controls = () => {
 };
 
 const PlayerWrapper = ({ children }: { children: React.ReactNode }) => {
-  const { togglePlay, isPlayerLoaded } = usePlayer();
+  const { isPlayerLoaded, containerRef } = usePlayer();
   return (
     <div
-      className="group relative mx-auto flex aspect-video max-h-[85vh] w-full justify-center overflow-hidden bg-black text-sm"
-      onClick={togglePlay}
+      ref={containerRef}
+      className="group relative mx-auto flex aspect-video max-h-[85vh] w-full justify-center overflow-hidden bg-black text-sm [&_video]:z-0"
     >
       {children}
       {isPlayerLoaded && <Controls />}
@@ -216,6 +281,8 @@ const Player = ({ url, previewImage, title }: VideoPlayerProps) => {
 };
 
 const VideoPlayer = (props: VideoPlayerProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
   const [state, setState] = useState<PlayerState>({
     // playing: true,
     playing: false,
@@ -228,12 +295,14 @@ const VideoPlayer = (props: VideoPlayerProps) => {
     loadedSeconds: 0,
     played: 0,
     playedSeconds: 0,
-    duration: props.duration
+    duration: props.duration,
+    isFullscreen: false
   });
 
   const { player } = state;
 
   const isPlayerLoaded = Boolean(state.player);
+  const title = props.title;
 
   const updateState = useCallback(<T extends keyof PlayerState>(key: T, value: PlayerState[T]) => {
     setState((prev) => ({ ...prev, [key]: value }));
@@ -274,6 +343,26 @@ const VideoPlayer = (props: VideoPlayerProps) => {
 
   const onDuration = useCallback((value: number) => updateState('duration', value), [updateState]);
 
+  const toggleFullscreen = useCallback(() => {
+    if (!containerRef.current) return;
+    if (screenfull.isEnabled) {
+      screenfull.toggle(containerRef.current, {
+        navigationUI: 'hide'
+      });
+    }
+  }, []);
+
+  // Fullscreen
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      updateState('isFullscreen', screenfull.isFullscreen);
+    };
+
+    screenfull.on('change', handleFullscreenChange);
+    return () => screenfull.off('change', handleFullscreenChange);
+  }, [updateState]);
+
+  // Key Bindings
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       switch (e.key.toLocaleLowerCase()) {
@@ -296,17 +385,22 @@ const VideoPlayer = (props: VideoPlayerProps) => {
         case 'j':
           backward();
           break;
+        case 'f':
+          toggleFullscreen();
+          break;
       }
     };
 
     document.addEventListener('keydown', handleKeyDown);
     return () => document.removeEventListener('keydown', handleKeyDown);
-  }, [togglePlay, togglePip, isPlayerLoaded, toggleLight, player, forward, backward]);
+  }, [togglePlay, togglePip, isPlayerLoaded, toggleLight, forward, backward, toggleFullscreen]);
 
   const value = useMemo(
     () => ({
       ...state,
       isPlayerLoaded,
+      title,
+      containerRef,
       onPause,
       onPlay,
       togglePlay,
@@ -318,11 +412,13 @@ const VideoPlayer = (props: VideoPlayerProps) => {
       forward,
       backward,
       onProgress,
-      onDuration
+      onDuration,
+      toggleFullscreen
     }),
     [
       state,
       isPlayerLoaded,
+      title,
       onPlay,
       onPause,
       togglePlay,
@@ -334,7 +430,8 @@ const VideoPlayer = (props: VideoPlayerProps) => {
       forward,
       backward,
       onProgress,
-      onDuration
+      onDuration,
+      toggleFullscreen
     ]
   );
 
